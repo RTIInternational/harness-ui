3935caa985224485f499b8227a462988e103063c
Michael Long <michaellong@rti.org>
2021-08-10 14:36:32 -0400

    Add classes and slots to FilterPicker

--- src/components/FilterPicker.vue
+++ src/components/FilterPicker.vue
@@ -1,60 +1,62 @@
 <template>
-  <div class="row">
-    <div :class="'col-' + columnSize">
-      <div class="row harness-ui-filtergrid-row">
-        <div class="col">
-          <label class="col-form-label">Add Filter</label>
-          <select
-            class="form-control"
-            name="filterPicker"
-            id="filterPicker"
-            @change="addFilter"
-          >
-            <option />
-            <option
-              v-for="filter in inactive"
-              :key="filter"
-              :value="filter"
+  <div class="harness-ui-filterpicker">
+    <div class="row">
+      <div :class="'col-' + columnSize">
+        <div class="row harness-ui-filtergrid-row">
+          <div class="col">
+            <slot class="harness-ui-filterpicker-label">Add Filter</slot>
+            <select
+              class="form-control harness-ui-filterpicker-select"
+              name="filterPicker"
+              id="filterPicker"
+              @change="addFilter"
             >
-              {{ getLabel(filter) }}
-            </option>
-          </select>
-        </div>
-        <div class="col text-center">
-          <label class="col-form-label">Remove Filter</label>
-          <select
-            class="form-control"
-            name="filterPicker"
-            id="filterPicker"
-            @change="removeFilter"
-          >
-            <option />
-            <option
-              v-for="filter in active"
-              :key="filter"
-              :value="filter"
+              <option />
+              <option
+                v-for="filter in inactive"
+                :key="filter"
+                :value="filter"
+              >
+                {{ getLabel(filter) }}
+              </option>
+            </select>
+          </div>
+          <div class="col text-center">
+            <slot class="harness-ui-filterpicker-label">Remove Filter</slot>
+            <select
+              class="form-control harness-ui-filterpicker-select"
+              name="filterPicker"
+              id="filterPicker"
+              @change="removeFilter"
             >
-              {{ getLabel(filter) }}
-            </option>
-          </select>
+              <option />
+              <option
+                v-for="filter in active"
+                :key="filter"
+                :value="filter"
+              >
+                {{ getLabel(filter) }}
+              </option>
+            </select>
+          </div>
         </div>
-      </div>
-      <div class="row button-row harness-ui-filtergrid-row harness-ui-filtergrid-buttonrow pt-3">
-        <div :class="'col-md-12 text-' + removeButtonPosition">
-          <button
-            role="button"
-            class="btn btn-primary btn-sm harness-ui-filtergrid-clearbutton"
-            @click="active=[]; initializeDefaults(); loadData();"
-          >
-            Remove All Filters
-          </button>
+        <div class="row button-row harness-ui-filtergrid-row harness-ui-filtergrid-buttonrow pt-3">
+          <div :class="'col-md-12 text-' + removeButtonPosition">
+            <button
+              role="button"
+              class="btn btn-primary btn-sm harness-ui-filtergrid-clearbutton"
+              @click="active=[]; initializeDefaults(); loadData();"
+            >
+              Remove All Filters
+            </button>
+          </div>
         </div>
+        <hr>
+        <FilterGrid
+          :only="active"
+          v-bind="{...$attrs}"
+        />
       </div>
-      <hr>
-      <FilterGrid
-        :only="active"
-        v-bind="{...$attrs}"
-      />
     </div>
   </div>
 </template>
